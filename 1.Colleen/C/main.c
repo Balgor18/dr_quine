aec778cd53d85709e83e33e9526b00f329a2f894
Florian Catinaud <catinaud.florian@gmail.com>
2023-07-13 21:09:17 +0200

    Some update in the main.c

--- 1.Colleen/C/main.c
+++ 1.Colleen/C/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define STRING "#include <stdio.h>%c%c#define STRING %c%s%c%c// This comment is outside%cvoid%cprint(const char *str)%c{%c%cprintf(str, 10, 10, 34, str, 34, 10, 10, 9, 10, 10, 9, 10, 10 , 9, 10 , 10, 9, 10, 9, 10, 10);%c}%cint%cmain()%c{%c%cprint(STRING);%c%creturn (0);%c}%c"
+#define STRING "#include <stdio.h>%2$c%2$c#define STRING %3$c%4$s%3$c%2$c// This comment is outside%2$cvoid%1$cprint(const char *str)%2$c{%2$c%1$cprintf(str, 9, 10, 34, str);%2$c}%2$cint%1$cmain()%2$c{%2$c%1$cprint(STRING);%2$c%1$creturn (0);%2$c}%2$c"
 // This comment is outside
 void	print(const char *str)
 {
-	printf(str, 10, 10, 34, str, 34, 10, 10, 9, 10, 10, 9, 10, 10 , 9, 10 , 10, 9, 10, 9, 10, 10);
+	printf(str, 9, 10, 34, str);
 }
 int	main()
 {
