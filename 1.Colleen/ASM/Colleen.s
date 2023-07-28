ccff5cb01ea79a22f9030215575e39140a589489
Florian Catinaud <catinaud.florian@gmail.com>
2023-07-28 23:34:14 +0200

    #7 Modif on all file for working on System Linux

--- 1.Colleen/ASM/Colleen.s
+++ 1.Colleen/ASM/Colleen.s
@@ -1,28 +1,24 @@
 bits 64
 section .data
 code :
-	.string db "section .data%1$ccode :%1$c	.string db %2$c%3$s%2$c%1$c%1$csection .text%1$c	extern _printf%1$c	global _main%1$c%1$c_print:%1$c	lea rdi, [rel code.string]%1$c	mov rsi, 10%1$c	mov rdx, 34%1$c;	This comment is inside%1$c	lea rcx, [rel code.string]%1$c	call _printf%1$c	mov rax, 0%1$c	ret%1$c%1$c; This comment is outside%1$c%1$c_main:%1$c	call _print%1$c	ret%1$c"
+	.string db "bits 64%1$csection .data%1$ccode :%1$c	.string db %2$c%3$s%2$c%1$c%1$csection .text%1$cextern printf%1$cglobal main%1$c%1$c_print:%1$c	lea rdi, [rel code.string]%1$c	mov rsi, 10%1$c	mov rdx, 34%1$c;	This comment is inside%1$c	lea rcx, [rel code.string]%1$c	call printf WRT ..plt%1$c	mov rax, 0%1$c	ret%1$c%1$c; This comment is outside%1$c%1$cmain:%1$c	call _print%1$c	ret%1$c"
 
 section .text
-	extern printf
-	global _main
-	global _start
+extern printf
+global main
+
 _print:
 	lea rdi, [rel code.string]
 	mov rsi, 10
 	mov rdx, 34
 ;	This comment is inside
 	lea rcx, [rel code.string]
-	call printf
+	call printf WRT ..plt
 	mov rax, 0
 	ret
 
 ; This comment is outside
 
-_main:
+main:
 	call _print
-	ret
-
-_start:
-	call _main
 	ret
